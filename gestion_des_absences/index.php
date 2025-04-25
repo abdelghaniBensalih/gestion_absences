57c0c4c2ca538cefeeafcf3ef5a3eb053fa9bdcd
Radouane <oukikredouane21@gmail.com>
2025-04-25 17:57:05 +0100

    hhhh

--- gestion_des_absences/index.php
+++ gestion_des_absences/index.php
@@ -3,6 +3,9 @@ $title = "Accueil";
 require_once "includes/header.php";
 require "config/db.php";
 
+header('Content-Type: text/html; charset=UTF-8');
+
+
 
 
 $error = "";
@@ -34,123 +37,9 @@ if (isset($_POST['subEtu'])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Accueil</title>
-    <link rel="stylesheet" href="assets/style.css">
+    <link rel="stylesheet" href="assetss/style.css">
+
     <style>
-        body {
-            background-color: white;
-            margin: 0px;
-            padding: 0px;
-            background-attachment: fixed;
-        }
-
-        .btns1 {
-            display: flex;
-            border: solid 4px #5da;
-            position: relative;
-            width: 500px;
-            height: 50px;
-            border-radius: 10px;
-            margin-top: 60px;
-            padding: 10px;
-            background-color: rgb(23, 211, 224);
-            justify-content: center;
-            align-items: center;
-        }
-
-        .btns1>form {
-            display: flex;
-            flex-direction: row;
-            width: 100%;
-            justify-content: space-around;
-        }
-
-        .btns1>form>input {
-            background-color: rgba(60, 110, 267);
-            width: 120px;
-            height: 50px;
-            margin: 10px;
-            border-radius: 5px;
-            border: none;
-            cursor: pointer;
-        }
-
-        .btns1>form>input:active {
-            background-color: rgba(240, 241, 267);
-            width: 120px;
-            height: 50px;
-            margin: 10px;
-            border-radius: 5px;
-            border: none;
-            cursor: pointer;
-        }
-
-        .etudiantConnecte {
-            border: solid 2px #0df;
-            position: relative;
-            width: 350px;
-            height: auto;
-            border-radius: 15px;
-            margin-top: 60px;
-            padding: 10px;
-            line-height: 42px;
-            background-color: rgba(240, 241, 267);
-            box-shadow: 40px 60px 60px #d3d3d3;
-        }
-
-        .adminConnecte {
-            border: solid 2px #0df;
-            position: relative;
-            width: 300px;
-            height: auto;
-            border-radius: 15px;
-            margin-top: 60px;
-            padding: 10px;
-            line-height: 42px;
-            background-color: rgba(241, 241, 267);
-            box-shadow: 40px 60px 60px #d3d3d3;
-        }
-
-        .adminConnecte>form>input {
-            text-indent: 5px;
-            background-color: white;
-            height: 26px;
-            width: 150px;
-            border: none;
-            margin: 0px;
-            padding: 0px;
-            border-radius: 7px;
-            border: 1px solid #0df;
-        }
-
-        .adminConnecte>form>input:focus {
-            text-indent: 5px;
-            height: 26px;
-            width: 150px;
-            margin: 0px;
-            padding: 0px;
-            border: 2px solid black;
-            background-color: rgba(245, 231, 244);
-            border-radius: 7px;
-            box-sizing: border-box;
-            border: 1px solid #0df;
-        }
-
-        .etudiantConnecte>form>#btn,
-        .adminConnecte>form>#btn {
-            cursor: pointer;
-            border: 1px solid #0df;
-            background-color: rgba(240, 241, 267);
-        }
-
-        .etudiantConnecte>form>input {
-            text-indent: 5px;
-            height: 26px;
-            width: 150px;
-            border: 1px solid #0df;
-            outline: none;
-            border-radius: 7px;
-
-        }
     </style>
 </head>
 
@@ -166,8 +55,8 @@ if (isset($_POST['subEtu'])) {
                 </form>
             </div>
 
-            
-            <?php if (isset($_POST['etudiant']) || $formuleAffiche): ?>     
+
+            <?php if (isset($_POST['etudiant']) || $formuleAffiche): ?>
                 <div class="etudiantConnecte">
                     <form action="" method="post">
                         <label for="ap">Entrer votre APOGEE: </label><br>
@@ -179,9 +68,9 @@ if (isset($_POST['subEtu'])) {
                     </form>
                     <p>vous avez pas encore inscrire? <a href="register.php">inscription</a></p>
                 </div>
-                <?php endif ?>
-                <?php if (isset($_POST['admin'])): ?>
-                    <div class="adminConnecte">
+            <?php endif ?>
+            <?php if (isset($_POST['admin'])): ?>
+                <div class="adminConnecte">
                     <form action="" method="post">
                         <label for="ap">Entrer votre identifiant: </label><br>
                         <input type="text" name="id" id="ap" placeholder='username'><br>
@@ -193,8 +82,6 @@ if (isset($_POST['subEtu'])) {
             <?php endif ?>
         </div>
     </center>
-
-
 </body>
 
 </html>
